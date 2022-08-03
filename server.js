934eb2949821e82f044939047b07e2a3d598ed3f
Andrew Blejde <snehp491@gmail.com>
2022-08-03 01:51:28 -0400

    add html routes and map into express

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Routes
+const htmlRoutes = require('./routes/htmlRoutes');
+
 // Configure server
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// Map the html routes
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API Server now on port ${PORT}`);
